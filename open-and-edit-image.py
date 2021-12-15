d60e7ad9b66b502b9eecac260219c863920cef04
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-12-15 01:47:35 -0300

    Adicionando arquivo "open-and-edit-image.py", que usa as bibliotecas "cv2" e "numpy" para manipular imagens.

---
+++ open-and-edit-image.py
@@ -0,0 +1,26 @@
+import cv2 
+import numpy as np
+
+#caminho da imagem
+imagePath = './rock.jpg'
+
+#link para a imagem utilizar - https://www.jbtc.com/foodtech/wp-content/uploads/sites/2/2021/08/Fruit-Vegetable-Processing.jpg
+
+#lendo a imagem 
+image = cv2.imread(imagePath)
+
+#criando image imagem do mesmo tamanho para ser o 'brilho'
+bright = np.zeros_like(image)
+#adicionaremos um brilho de 100 em cada pixel
+bright[:,:,:] = [100,100,100] # (b,g,r)
+#some 100 em cada pixel da imagem
+brilho = cv2.add(image, bright)
+#caso a soma ultrapasse 255 (máximo), limite-o a 255 (branco)
+brilho[brilho > 255] = 255
+
+#mostre a imagem original
+cv2.imshow('image', image)
+#mostre a imagem de brilho
+cv2.imshow('brilho', brilho)
+#espere um comando
+cv2.waitKey()
